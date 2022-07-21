c88c6dbb8e61a3e692bb775782122b7ca7791b41
SuS <sus.webdev@gmail.com>
2022-07-21 16:16:05 +0200

    1.2

--- js/main.js
+++ js/main.js
@@ -1,80 +1,102 @@
-
-let body = document.querySelector("body");
+let intro = document.querySelector(".intro");
 for (let i = 0; i < 50; i++) {
   let star = document.createElement("span");
   star.setAttribute("class", "star");
   let posX = Math.floor(Math.random() * 90) + "%";
   let posY = Math.floor(Math.random() * 90) + "%";
   star.style = `top:${posX};left:${posY}`;
-  body.append(star);
+  intro.append(star);
 }
 
 gsap.to(".star", {
-    opacity: .1,
-    duration: 1,
-    repeat: -1,
-    yoyo: true,
-    ease: "linear"
-  });
-  
+  opacity: 0.1,
+  duration: 1,
+  repeat: -1,
+  yoyo: true,
+  ease: "linear",
+});
 
+//   Clouds
 
+gsap.to(".cloud-1", {
+  x: 100,
+  duration: 8,
+  repeat: -1,
+  ease: "none",
+  yoyo: true,
+});
 
+gsap.to(".cloud-2", {
+  x: 100,
+  duration: 7,
+  repeat: -1,
+  ease: "none",
+  yoyo: true,
+});
 
+gsap.to(".cloud-3", {
+  x: 100,
+  duration: 10,
+  repeat: -1,
+  ease: "none",
+  yoyo: true,
+});
 
-
-  let screenWidth = window.innerWidth + "px";
-  let negativesSreenWidth ="-" + window.innerWidth + "px";
-
-//   clouds
-
-gsap.set(".cloud-3",{bottom:"30%",immediateRender:true})
-gsap.set(".cloud-2",{left:"30%",immediateRender:true})
-
-gsap.fromTo(".cloud-1",{
-left:screenWidth,
-},{   
-    left:negativesSreenWidth,
-    duration:400,
-    repeat:-1,
-    ease:Power0,
-    delay:10
-     
-})
-
-gsap.to(".cloud-2",{
-    left:negativesSreenWidth,
-    duration:300,
-    yoyo:true,
-    repeat:-1,
-    ease:Power0,
-})
-
-gsap.fromTo(".cloud-3",{
-    left:screenWidth,
-    },{
-        left:negativesSreenWidth,
-        duration:400,
-        repeat:-1,
-        ease:Power0,
-        
-    })
-
-// title
+// Title
 
 let tl = gsap.timeline();
 
-    tl.from(".title",{
-        opacity:0,
-        y:"200px",
-        ease:"power4.inOut",
-        duration:3
-      },"<")
+gsap.from(".redirect-btn", {
+  y: 30,
+  repeat: -1,
+  yoyo: true,
+  ease: "power4.inOut",
+  duration: 2,
+});
 
-    tl.from(".desc",{
-        opacity:0,
-        x:50,
-        delay:1,
-        ease:"power4.inOut",
-        duration:2
-      },"<")
+// preloader
+
+window.addEventListener("load", () => {
+  gsap.to("#preloader", {
+    opacity: 0,
+    zIndex: "-1",
+    ease: "power4.inOut",
+    duration: 1,
+  });
+
+  setTimeout(() => {
+    document.getElementById("preloader").remove();
+  }, 1000);
+
+  tl.from(
+    ".title",
+    {
+      opacity: 0,
+      y: "200px",
+      ease: "power4.inOut",
+      duration: 2,
+    },
+    "<"
+  );
+  tl.from(
+    ".subtitle",
+    {
+      opacity: 0,
+      y: "100px",
+      ease: "power4.inOut",
+      duration: 3,
+    },
+    "<"
+  );
+
+  tl.from(
+    ".desc",
+    {
+      opacity: 0,
+      y: 50,
+      ease: "power4.inOut",
+      duration: 2,
+    },
+    "<.5"
+  );
+});
